e70290a5c93c846bcde37bf79abe38e533daa91f
German Salyukin <shuriksful@gmail.com>
2019-07-29 20:37:11 +0400

    Создание приложения

---
+++ src/components/list.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class List extends React.Component{
+    render(){
+        return (
+            <div>
+                Список итемов
+            </div>
+        );
+    }
+}
+
+export default List;
