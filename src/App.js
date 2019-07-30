1a604d901be21b3b882237817cd94bbea1222d82
Herman <shuriksful@gmail.com>
2019-07-31 02:29:56 +0400

    Вывод списком.

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
     const city = e.target.elements.city.value;
 
     if(city) {
-      const api_url = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`);
+      const api_url = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`);
       const data = await api_url.json();
 
       if (data.cod == "200") {
--- src/components/itemList.js
+++ src/components/itemList.js
@@ -1,38 +1,89 @@
 import React from 'react'
-import { Row }from 'reactstrap'
+import {Collapse} from 'reactstrap'
 
 const date = new Date();
+const weekName = [
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота',
+  'Воскресенье',
+];
 
-const ItemList = (props) => {
-  var item = props.item,
-      date = new Date();
-  date.setTime(item.dt);
+class ItemList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.item = this.props.item;
+    this.date = new Date(this.item.dt * 1000);
+    this.getRgbColorByTemp(this.item.main.temp);
+    this.toggle = this.toggle.bind(this);
+    this.state = { collapse: false };
+  }
 
-  return(
-    <Row className='list_item'>
-      <div className='row'>
-        <div className='col-6'>Дата: </div><div className='col-6'>{item.dt}</div>
+  getRgbColorByTemp(temp) {
+    let rgb = [223, 223, 223];
+
+    if (temp >= 25) {
+      rgb =[223, 76 + Math.round(147 * (temp - 25) / 25) , 76]
+    }
+    if (temp < 25 && temp >= 0) {
+      rgb =[76 + Math.round(147 * temp / 25) ,223, 76]
+    }
+    if (temp < 0 && temp >= -25) {
+      rgb =[76, 223, 76 + Math.round(147 * (temp + 25) / 25)]
+    }
+    if (temp < -25) {
+      rgb =[76, 76 + Math.round(147 * (temp + 50) / 25) ,223]
+    }
+
+    this.rgb = rgb;
+  }
+
+  toggle() {
+    this.setState(state => ({ collapse: !state.collapse }));
+  }
+
+  render(){
+    return (
+      <div className='list_item'>
+        {/*<button onClick={this.toggle}>Давай уже</button>*/}
+        <div className='item__header d-flex flex-row' onClick={this.toggle} style={{backgroundColor: `rgb(${this.rgb[0]}, ${this.rgb[1]}, ${this.rgb[2]})`}}>
+          <div className='p-1 item__date'>
+            <span className='text__date text-light'>{this.date.getUTCDate()}</span>
+          </div>
+          <div className='p-1 item__day text-secondary'>
+            {weekName[this.date.getDay()]} <b>{this.date.getUTCHours()}:00</b>
+          </div>
+        </div>
+        <Collapse isOpen={this.state.collapse}>
+          <div className='item__content'>
+            <div className='row'>
+              <div className='col-6'>Температура (C°):</div>
+              <div className='col-6'>{this.item.main.temp}</div>
+            </div>
+            <div className='row'>
+              <div className='col-6'>Атмосферное давление (гПа):</div>
+              <div className='col-6'>{this.item.main.pressure}</div>
+            </div>
+            <div className='row'>
+              <div className='col-6'>Влажность (%):</div>
+              <div className='col-6'>{this.item.main.humidity}</div>
+            </div>
+            <div className='row'>
+              <div className='col-6'>Осадки:</div>
+              <div className='col-6'>{this.item.weather.main}</div>
+            </div>
+            <div className='row'>
+              <div className='col-6'>Облачность (%):</div>
+              <div className='col-6'>{this.item.clouds.all}</div>
+            </div>
+          </div>
+        </Collapse>
       </div>
-      <div className='row'>
-        <div className='col-6'>Температура (Кельвин): </div><div className='col-6'>{item.dt}</div>
-      </div>
-      <div className='row'>
-        <div className='col-6'>Атмосферное давление (гПа): </div><div className='col-6'>{item.main.pressure}</div>
-      </div>
-      <div className='row'>
-        <div className='col-6'>Влажность (%): </div><div className='col-6'>{item.main.humidity}</div>
-      </div>
-      <div className='row'>
-        <div className='col-6'>Осадки: </div><div className='col-6'>{item.weather.main}</div>
-      </div>
-      <div className='row'>
-        <div className='col-6'>Облачность (%): </div><div className='col-6'>{item.clouds.all}</div>
-      </div>
-      <div className='row'>
-        <div className='col-6'>Облачность (%): </div><div className='col-6'>{item.clouds.all}</div>
-      </div>
-    </Row>
-  );
+    );
+  }
 };
 
 export default ItemList;
